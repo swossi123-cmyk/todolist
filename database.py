e3d9c9f9e0598926743110a65cf03d445062344b
Abdelkabir NL2 <swossi123@gmail.com>
2026-07-03 12:24:11 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,10 @@
-from sqlalchemy import create_engine 
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-
+# قاعدة البيانات ف الذاكرة المؤقتة لـ Vercel
 DB_URL = "sqlite:///:memory:"
 
-engine = create_engine(DB_URL, 
-                       connect_args={"check_same_thread":False})
-
-SessionLocal = sessionmaker(bind=engine,
-                             autoflush=False,
-                               autocommit=False)
-
-Base = declarative_base() 
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
